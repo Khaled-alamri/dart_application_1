81d52bf22edbf2caac8c698b2b8068cd6b276aca
khaled <khaled1422_8@gmail.com>
2022-11-10 15:05:14 +0300

    lab dart.1

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,8 +1,72 @@
-import 'dart:html';
 import 'dart:io';
 
 main() {
+  //how to print in dart.
   print("khaled");
   print("hi");
   stdout.write("hhhhhh");
+
+  //how to ues Variable in dart .
+
+  const daet_of_bithday = '1422 / 8 / 11';
+
+  int age = 21;
+
+  String jop = "towaiq";
+
+  String time_of_jop = "10AM to 3AM";
+
+  var car = "Nasan";
+//num useing all teyp of nambur.
+  num phone = 0557403701;
+  //null sf
+  bool? student;
+
+  //how to print variable .
+
+  print(name);
+
+  //how to print 2 variable in you want in m print.
+
+  print("me name is $name me age is $age");
+  //in this way moust by same datatype .
+  print(name + " " + daet_of_bithday);
+  print(car);
+  print(time_of_jop);
+  print(phone);
+  print(Addres);
+
+
+int dart_of_date = 2011;
+int javascript_of_date = 1996;
+
+bool dart_web = true;
+bool javascript_web = true;
+
+bool dart_app = true;
+bool javascript_app = false;
+
+int dart_use = 5;
+int javascript_use = 1;
+
+bool dart_game = false;
+bool javascript_game = true;
+
+
+
+
+  print(dart_of_date < javascript_of_date);
+  print(dart_web && javascript_web);
+  print(javascript_use != dart_use);
+  print(dart_app || javascript_app);
+  print(dart_game && javascript_game);
+
 }
+
+//you can create variable out the main.
+var name = "khaled";
+//dynamic O way to use it?
+//if you want take iny datatayp
+dynamic Addres = "abha";
+
+
